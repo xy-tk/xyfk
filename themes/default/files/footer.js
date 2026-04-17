6dd98bbeefb0aa80e82a984c5f734081ea89ee0a
xzek <137222445@qq.com>
2026-04-17 11:55:08 +0800

    Update footer.js

--- themes/default/files/footer.js
+++ themes/default/files/footer.js
@@ -39,7 +39,7 @@ function insertFooterSkeleton() {
 				<div class="footer-links">
 					<a href="" target="_blank"><i class="fab fa-qq"></i> 137222445</a><span class="split" style="margin: 0 3px;">|</span><a href="" target="_blank"><i class="fab fa-telegram-plane"></i> @gv1688</a><span class="split" style="margin: 0 3px;">|</span><a href="" target="_blank"><i class="fas fa-rss"></i> </a>
 				</div>
-				<p></p><p>Copyright @ 2026 <a href="/" target="_blank">XYFK </a> - 基于 Cloudflare 构建</p>
+				<p>Copyright @ 2026 <a href="/" target="_blank">XYFK </a> - 基于 Cloudflare 构建</p>
 			</div>
 			</div>
 	</footer>
